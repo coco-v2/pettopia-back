6facc32f175170d0674c91dc5e49dbb97614d55d
eunslog <yeseo235@gmail.com>
2024-03-31 17:48:11 +0900

    초기세팅

---
+++ src/main/java/org/pettopia/pettopiaback/PettopiaBackApplication.java
@@ -0,0 +1,13 @@
+package org.pettopia.pettopiaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettopiaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PettopiaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pettopia/pettopiaback/PettopiaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pettopia.pettopiaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettopiaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
