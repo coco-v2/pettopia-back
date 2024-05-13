bc559dd42e7f1fcaecbdd9846e460e82fc8c6eb1
eunslog <yeseo235@gmail.com>
2024-05-13 20:37:35 +0900

    feat/getPetInfo

--- src/main/java/org/pettopia/pettopiaback/controller/PetController.java
+++ src/main/java/org/pettopia/pettopiaback/controller/PetController.java
@@ -5,13 +5,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.pettopia.pettopiaback.domain.Users;
 import org.pettopia.pettopiaback.dto.PetDTO;
+import org.pettopia.pettopiaback.dto.PrincipalDetail;
 import org.pettopia.pettopiaback.service.PetService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,8 +27,10 @@ public class PetController {
     private final PetService petService;
 
     @PostMapping("/info/")
-    public ResponseEntity makePetInfo(String userId, @RequestBody @Valid PetDTO.AddPetInfoRequest addPetInfoRequest
+    public ResponseEntity makePetInfo(@AuthenticationPrincipal PrincipalDetail userDetails
+            , @RequestBody @Valid PetDTO.AddPetInfoRequest addPetInfoRequest
     ) throws RuntimeException {
+        String userId = (String) userDetails.getMemberInfo().get("socialId");
 
         petService.makePetInfo(userId, addPetInfoRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -31,11 +38,34 @@ public class PetController {
     }
 
     @GetMapping("/info/")
-    public ResponseEntity<PetDTO.PetInfoResponse> getPetInfo(String userId){
+    public ResponseEntity<PetDTO.PetInfoListResponse> getPetInfoList(
+            @AuthenticationPrincipal PrincipalDetail userDetails
+    ) throws RuntimeException {
 
-        PetDTO.PetInfoResponse petInfoResponse = petService.getPetInfo(userId);
+        String userId = (String) userDetails.getMemberInfo().get("socialId");
 
-        return ResponseEntity.ok(petInfoResponse);
+        List<PetDTO.PetInfoResponse> petInfoResponses
+                = petService.getPetInfoList(userId);
+
+        PetDTO.PetInfoListResponse response = PetDTO.PetInfoListResponse.builder()
+                .cnt(petInfoResponses.size())
+                .list(petInfoResponses)
+                .build();
+
+        return ResponseEntity.ok(response);
     }
+//    public ResponseEntity<PetDTO.PetInfoResponse> getPetInfo(
+//            @AuthenticationPrincipal PrincipalDetail userDetails
+//    ) throws RuntimeException {
+//
+//        String userId = (String) userDetails.getMemberInfo().get("socialId");
+//
+//
+//        PetDTO.PetInfoResponse petInfoResponse = petService.getPetInfo(userId);
+//
+//        return ResponseEntity.ok(petInfoResponse);
+//    }
+//
+
 
 }
--- src/main/java/org/pettopia/pettopiaback/repository/PetRepository.java
+++ src/main/java/org/pettopia/pettopiaback/repository/PetRepository.java
@@ -4,8 +4,11 @@ import org.pettopia.pettopiaback.domain.Pet;
 import org.pettopia.pettopiaback.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
     Pet findByUsers(Users user);
+    List<Pet> findAllByUsers(Users user);
 
 }
